516ac6411c6e9fcc32bc14aaaf2f1473f4d98482
isimsek7 <isimsekmusic@gmail.com>
2024-08-08 16:59:09 +0300

    Add files via upload

---
+++ BaseGeometricShapes.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace PolyMorph
+{
+	public class BaseGeometricShapes
+	{
+		public BaseGeometricShapes()
+		{
+		}
+		public int _width { get; set; }
+		public int _length { get; set; }
+
+		public virtual void CalcArea()
+		{
+			Console.WriteLine("Area is " + _width * _length + " centimeters.");
+		}
+	}
+	public class Square : BaseGeometricShapes
+	{
+
+		public Square(int w)
+		{
+			_width = w;
+			_length = w;
+		}
+
+
+
+	}
+	public class Rectangle : BaseGeometricShapes
+	{
+
+		public Rectangle(int w, int l)
+		{
+			_width = w;
+			_length = l;
+		}
+	}
+	public class RightTriangle : BaseGeometricShapes
+	{
+
+		public RightTriangle(int w, int l)
+		{
+			_width = w;
+			_length = l;
+		}
+		public override void CalcArea()
+		{
+			Console.WriteLine("Area is " + (_width * _length) / 2 + " centimeters.");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using PolyMorph;
+Square square1 = new Square(10);
+square1.CalcArea();
+Rectangle rec1 = new Rectangle(10, 5);
+rec1.CalcArea();
+RightTriangle rt1 = new RightTriangle(10, 20);
+rt1.CalcArea();
